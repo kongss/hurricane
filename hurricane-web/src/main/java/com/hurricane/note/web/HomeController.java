d7e7701b0759a905407a76199c6a06f908c8d784
649074622 <649074622@qq.com>
2017-11-30 11:02:32 +0800

    web 起不来

--- hurricane-web/src/main/java/com/hurricane/note/web/HomeController.java
+++ hurricane-web/src/main/java/com/hurricane/note/web/HomeController.java
@@ -1,5 +1,8 @@
 package com.hurricane.note.web;
 
+import com.hurricane.note.api.DUserService;
+import com.hurricane.note.utils.bean.MessengerVo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,8 +14,14 @@ public class HomeController {
     @Value("${name}")
     String sp;
 
+    @Autowired
+    DUserService dUserService;
+
     @RequestMapping("/")
     @ResponseBody String home(){
+        MessengerVo vo = new MessengerVo();
+        MessengerVo info = dUserService.getUserInfo(vo);
+        info.getString("ttttt");
         return "Hello "+sp ;
     }
 }
