0cd8588baf6e108279e2cadba4ddfa8668e8fd93
649074622 <649074622@qq.com>
2017-12-01 16:26:11 +0800

    接口连通

--- hurricane-web/src/main/java/com/hurricane/HurricaneWebApplication.java
+++ hurricane-web/src/main/java/com/hurricane/HurricaneWebApplication.java
@@ -2,7 +2,9 @@ package com.hurricane;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 
+@ImportResource("classpath:consumer/hurricane-service-consumer.xml")
 @SpringBootApplication
 public class HurricaneWebApplication {
 
