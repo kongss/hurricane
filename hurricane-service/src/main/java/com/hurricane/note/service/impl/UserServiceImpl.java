baa1bf42af14e924a6b36398b001fbc26a9f1418
649074622 <649074622@qq.com>
2017-12-06 14:26:51 +0800

    成功插入用户信息

--- hurricane-service/src/main/java/com/hurricane/note/service/impl/UserServiceImpl.java
+++ hurricane-service/src/main/java/com/hurricane/note/service/impl/UserServiceImpl.java
@@ -6,13 +6,18 @@ import com.hurricane.note.service.UserService;
 import com.hurricane.note.utils.bean.HConstants;
 import com.hurricane.note.utils.bean.MessengerVo;
 
+import com.hurricane.note.utils.utils.HuUUID;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.UUID;
+
 @Service
 public class UserServiceImpl implements UserService{
+
+    Logger logger = LogManager.getLogger(UserServiceImpl.class);
 
     @Autowired
     UserMapper userMapper;
@@ -23,7 +28,6 @@ public class UserServiceImpl implements UserService{
      * @return
      */
     public MessengerVo getUserInfo(MessengerVo messenger) {
-        Logger logger = LogManager.getLogger(UserServiceImpl.class);
         User user = userMapper.selectByPrimaryKey("14d7d3b2-c2ac-44c4-859f-5e858efa0223");
         logger.info("nickname:"+user.getNickname());
         messenger.setInfo("nickname",user.getNickname());
@@ -36,7 +40,9 @@ public class UserServiceImpl implements UserService{
      * @return
      */
     public MessengerVo saveUser(MessengerVo messenger) {
+        logger.info("UserServiceImpl-saveUser-messenger:",messenger);
         User user = new User();
+        user.setUuid(HuUUID.getUuid());
         int i = userMapper.insertSelective(user);
         if (i>0){
             messenger.setResCode(HConstants.SUCCESS);
