4644f612a7b4d73c3d1e94479dc2226b8b417bdc
Administrator <649074622@qq.com>
2017-12-07 23:10:58 +0800

    admin 改为springboot项目

---
+++ hurricane-admin/src/main/java/com/hurricane/HurricaneAdminApplication.java
@@ -0,0 +1,12 @@
+package com.hurricane;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HurricaneAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HurricaneAdminApplication.class, args);
+	}
+}
