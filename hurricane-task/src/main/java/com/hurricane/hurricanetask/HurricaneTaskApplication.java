a3bac73ab5f9ea5e0c6aec49a48db7130c4a7a53
649074622 <649074622@qq.com>
2017-12-27 14:14:27 +0800

    task 项目 初始化上传

---
+++ hurricane-task/src/main/java/com/hurricane/hurricanetask/HurricaneTaskApplication.java
@@ -0,0 +1,12 @@
+package com.hurricane.hurricanetask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HurricaneTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HurricaneTaskApplication.class, args);
+	}
+}
