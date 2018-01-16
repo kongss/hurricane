84e1dae76f9203985a8078b27e6a2ead3d5aac3f
649074622 <649074622@qq.com>
2018-01-16 15:31:29 +0800

    获取商城详情信息接口

--- hurricane-web/src/main/java/com/hurricane/coupon/web/SellerController.java
+++ hurricane-web/src/main/java/com/hurricane/coupon/web/SellerController.java
@@ -32,6 +32,14 @@ public class SellerController {
         messenger = dSellerService.getSellerSortList(messenger);
         return messenger;
     }
+
+    /**
+     * 商城推荐列表接口
+     * @param limitStart
+     * @param limitSize
+     * @param sellerTypeUuid
+     * @return
+     */
     @ResponseBody
     @RequestMapping(value = "/seller/sellerRecomList", method = {RequestMethod.GET,RequestMethod.POST})
     public MessengerVo sellerRecomList(@RequestParam (defaultValue = "0") Integer limitStart, @RequestParam (defaultValue = "20") Integer limitSize, @RequestParam(required = true) Integer sellerTypeUuid){
@@ -39,6 +47,14 @@ public class SellerController {
         messenger.setInfo("limitStart", limitStart);
         messenger.setInfo("limitSize",limitSize);
         messenger = dSellerService.getSellerRecomList(messenger);
+        return messenger;
+    }
+    @ResponseBody
+    @RequestMapping(value = "/seller/sellerInfo", method = {RequestMethod.GET,RequestMethod.POST})
+    public MessengerVo sellerInfo(@RequestParam(required = true) Integer uuid){
+        MessengerVo messenger = new MessengerVo();
+        messenger.setInfo("uuid",uuid);
+        messenger = dSellerService.getSellerInfo(messenger);
         return messenger;
     }
 }
