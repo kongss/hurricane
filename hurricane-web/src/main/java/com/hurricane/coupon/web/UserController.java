b65cf39ccfe42a1499a48ed7af9ad79a341d5ffb
649074622 <649074622@qq.com>
2018-01-16 15:34:28 +0800

    获取个人信息接口

--- hurricane-web/src/main/java/com/hurricane/coupon/web/UserController.java
+++ hurricane-web/src/main/java/com/hurricane/coupon/web/UserController.java
@@ -1,8 +1,26 @@
 package com.hurricane.coupon.web;
 
+import com.hurricane.coupon.api.DUserService;
+import com.hurricane.coupon.utils.bean.MessengerVo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class UserController {
 
+    @Autowired
+    DUserService dUserService;
+
+    @ResponseBody
+    @RequestMapping(value = "/user/userInfo", method = {RequestMethod.GET,RequestMethod.POST})
+    public MessengerVo sellerInfo(@RequestParam(required = true) Integer openId){
+        MessengerVo messenger = new MessengerVo();
+        messenger.setInfo("openId",openId);
+        messenger = dUserService.getUserInfoByOpenId(messenger);
+        return messenger;
+    }
 }
