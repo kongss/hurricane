8144485271f74ba992796a893dd2e540c3ca673d
kongshuaishuai_Pro <629074622@qq.com>
2019-01-14 11:07:01 +0800

    jdbc链接测试类

---
+++ hurricane-dao/src/test/java/com/hurricane/JdbcConnTest.java
@@ -0,0 +1,68 @@
+package com.hurricane;
+
+import java.sql.*;
+
+public class JdbcConnTest {
+    // JDBC 驱动名及数据库 URL
+    static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
+    static final String DB_URL = "jdbc:mysql://101.200.235.166:3306/notedb";
+
+    // 数据库的用户名与密码，需要根据自己的设置
+    static final String USER = "root";
+    static final String PASS = "root";
+
+    public static void main(String[] args) {
+        Connection conn = null;
+        Statement stmt = null;
+        try{
+            // 注册 JDBC 驱动
+            Class.forName(JDBC_DRIVER);
+
+            // 打开链接
+            System.out.println("连接数据库...");
+            conn = DriverManager.getConnection(DB_URL,USER,PASS);
+
+            // 执行查询
+            System.out.println(" 实例化Statement对象...");
+            stmt = conn.createStatement();
+            String sql;
+            sql = "SELECT uuid, name, start_time FROM coupon";
+            ResultSet rs = stmt.executeQuery(sql);
+
+            // 展开结果集数据库
+            while(rs.next()){
+                // 通过字段检索
+                String uuid  = rs.getString("uuid");
+                String name = rs.getString("name");
+                String start_time = rs.getString("start_time");
+
+                // 输出数据
+                System.out.println("uuid: " + uuid);
+                System.out.println("name: " + name);
+                System.out.println("start_time: " + start_time);
+            }
+            // 完成后关闭
+            rs.close();
+            stmt.close();
+            conn.close();
+        }catch(SQLException se){
+            // 处理 JDBC 错误
+            se.printStackTrace();
+        }catch(Exception e){
+            // 处理 Class.forName 错误
+            e.printStackTrace();
+        }finally{
+            // 关闭资源
+            try{
+                if(stmt!=null) stmt.close();
+            }catch(SQLException se2){
+            }// 什么都不做
+            try{
+                if(conn!=null) conn.close();
+            }catch(SQLException se){
+                se.printStackTrace();
+            }
+        }
+        System.out.println("Goodbye!");
+    }
+}
